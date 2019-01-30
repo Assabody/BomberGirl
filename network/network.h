7d8789fc24dd4d3e6306b2ef9eb31e90bf13eb55
Jerome Daurat <jeje78660@yahoo.fr>
2019-01-30 13:37:10 -0500

    select multi accept

---
+++ network/network.h
@@ -0,0 +1,15 @@
+#ifndef _NETWORK_H_
+#define _NETWORK_H_
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+#include <sys/time.h>
+
+#endif //_NETWORK_H_
