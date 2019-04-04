74e19189b7d17e31681764a8f280864bcc409c2d
Jerome Daurat <jeje78660@yahoo.fr>
2019-04-04 13:49:52 -0400

    fix HUD position

--- sources/drawing.c
+++ sources/drawing.c
@@ -1,6 +1,7 @@
 #include "../includes/main.h"
 
-void drawMap(game_t *game) {
+void drawMap(game_t *game)
+{
     SDL_Rect position;
     int x = 0;
     int y = 0;
@@ -8,8 +9,10 @@ void drawMap(game_t *game) {
     position.y = 0;
     position.w = 40;
     position.h = 40;
-    while (y < Y_MAP_SIZE) {
-        while (x < X_MAP_SIZE) {
+    while (y < Y_MAP_SIZE)
+    {
+        while (x < X_MAP_SIZE)
+        {
             switch (get_cell_type(game->map[y][x].cell))
             {
             case MAP_WALL_UNBREAKABLE:
@@ -23,24 +26,27 @@ void drawMap(game_t *game) {
                 break;
             }
 
-            if (is_flame(game->map[y][x].cell)) {
+            if (is_flame(game->map[y][x].cell))
+            {
                 SDL_RenderCopy(game->sdl->renderer, game->textures->flame, NULL, &position);
-            } else if (is_bomb(game->map[y][x].cell)) {
+            }
+            else if (is_bomb(game->map[y][x].cell))
+            {
                 drawBombs(game, position);
             }
             else if (is_bonus(game->map[y][x].cell) && get_cell_type(game->map[y][x].cell) == MAP_GRASS)
             {
                 switch (get_bonus(game->map[y][x].cell))
                 {
-                    case BOMB_NUMBER_BONUS:
-                        SDL_RenderCopy(game->sdl->renderer, game->textures->bomb_bonus, NULL, &position);
-                        break;
-                    case SPEED_BONUS:
-                        SDL_RenderCopy(game->sdl->renderer, game->textures->speed_bonus, NULL, &position);
-                        break;
+                case BOMB_NUMBER_BONUS:
+                    SDL_RenderCopy(game->sdl->renderer, game->textures->bomb_bonus, NULL, &position);
+                    break;
+                case SPEED_BONUS:
+                    SDL_RenderCopy(game->sdl->renderer, game->textures->speed_bonus, NULL, &position);
+                    break;
                 }
             }
-                x++;
+            x++;
             position.x += 40;
         }
         x = 0;
@@ -50,16 +56,22 @@ void drawMap(game_t *game) {
     }
 }
 
-void drawBombs(game_t *game, SDL_Rect position) {
+void drawBombs(game_t *game, SDL_Rect position)
+{
     int x;
     int y;
     player_coords_to_map_coords(position.x, position.y, &x, &y);
     int bomb_clip = 0;
-    if (game->map[y][x].duration > 80 && game->map[y][x].duration <= 160) {
+    if (game->map[y][x].duration > 80 && game->map[y][x].duration <= 160)
+    {
         bomb_clip = 2;
-    } else if (game->map[y][x].duration > 5 && game->map[y][x].duration <= 80) {
+    }
+    else if (game->map[y][x].duration > 5 && game->map[y][x].duration <= 80)
+    {
         bomb_clip = 1;
-    } else if (game->map[y][x].duration > 0 && game->map[y][x].duration < 5) {
+    }
+    else if (game->map[y][x].duration > 0 && game->map[y][x].duration < 5)
+    {
         bomb_clip = 3;
     }
     game->map[y][x].duration--;
@@ -71,8 +83,10 @@ void drawBombs(game_t *game, SDL_Rect position) {
         &game->textures->bomb_clips[bomb_clip]);
 }
 
-void drawPlayer(game_t *game) {
-    for (int i = 0; i < MAX_PLAYERS; i++) {
+void drawPlayer(game_t *game)
+{
+    for (int i = 0; i < MAX_PLAYERS; i++)
+    {
         if (game->player[i].alive)
         {
             renderTexture(
@@ -85,7 +99,8 @@ void drawPlayer(game_t *game) {
     }
 }
 
-void disp_text(SDL_Renderer *renderer, char *text, TTF_Font *font, int x, int y) {
+void disp_text(SDL_Renderer *renderer, char *text, TTF_Font *font, int x, int y)
+{
     SDL_Surface *surface;
     SDL_Texture *texture;
     SDL_Rect pos;
@@ -107,23 +122,38 @@ void drawHUD(game_t *game)
     SDL_Texture *texture;
     SDL_Rect pos;
     SDL_Color color = {255, 255, 0, 255};
-    int x = 0;
-    int y = 5;
-    char message[10];
+    char message[7];
     for (int i = 0; i < MAX_PLAYERS; i++)
-      {
-	if (game->player[i].alive)
-	  {
-	    sprintf(message, "PV J%d: %d", i+1, game->player[i].life);
-	    surface = TTF_RenderText_Solid(game->sdl->font, message, color);
-	    texture = SDL_CreateTextureFromSurface(game->sdl->renderer, surface);
-	    pos.w = surface->w;
-	    pos.h = surface->h;
-	    pos.x = x ;
-	    pos.y = y;
-	    SDL_FreeSurface(surface);
-	    SDL_RenderCopy(game->sdl->renderer, texture, NULL, &pos);
-	  }
-	x += 70;
-      }
+    {
+        if (game->player[i].alive)
+        {
+            sprintf(message, "PV %d", game->player[i].life);
+            surface = TTF_RenderText_Solid(game->sdl->font, message, color);
+            texture = SDL_CreateTextureFromSurface(game->sdl->renderer, surface);
+            pos.w = surface->w;
+            pos.h = surface->h;
+            switch (i)
+            {
+            case 0:
+                pos.x = 5;
+                pos.y = 5;
+                break;
+            case 1:
+                pos.x = SCREEN_SIZE_X - surface->w - 5;
+                pos.y = 5;
+                break;
+            case 2:
+                pos.x = 5;
+                pos.y = SCREEN_SIZE_Y - surface->h - 5;
+                break;
+            case 3:
+                pos.x = SCREEN_SIZE_X - surface->w - 5;
+                pos.y = SCREEN_SIZE_Y - 30;
+                break;
+            }
+            SDL_FreeSurface(surface);
+            SDL_RenderCopy(game->sdl->renderer, texture, NULL, &pos);
+            SDL_DestroyTexture(texture);
+        }
+    }
 }
