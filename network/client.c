0d9a0e492dc3db13cf8fc3366492da17d8c7ad07
MatthiasPouget <pouget_m@etna-alternance.net>
2019-04-04 16:47:39 +0200

    gestion HUD3

--- network/client.c
+++ network/client.c
@@ -32,8 +32,9 @@ int getServerInfo(int socket, game_t *game)
                     game->player[i].current_speed = game_infos.players[i].current_speed;
                     game->player[i].max_speed = game_infos.players[i].max_speed;
                     game->player[i].bombs_left = game_infos.players[i].bombs_left;
-                    game->player[i].bombs_capacity = game_infos.players[i].bombs_capacity;
-                }
+		    game->player[i].bombs_capacity = game_infos.players[i].bombs_capacity;
+                    game->player[i].life = game_infos.players[i].life;
+		}
 
             }
             return 1;
